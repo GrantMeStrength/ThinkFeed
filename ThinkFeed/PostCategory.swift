2d032ceb18ff2550d99efdbd45ad42379f1dea1e
John Kennedy <john_kenn@mac.com>
2025-04-21 11:07:59 -0700

    more fixes

--- ThinkFeed/PostCategory.swift
+++ ThinkFeed/PostCategory.swift
@@ -9,13 +9,13 @@ import Foundation
 
 enum PostCategory: String, Codable, CaseIterable {
     case app = "ThinkFeed"
- //   case technology = "Technology"
+    case technology = "Technology"
     case science = "Science"
-   // case health = "Health"
+   case health = "Health"
     case business = "Business"
     case arts = "Arts"
     case math = "Math"
-   // case education = "Education"
+    case education = "Education"
     case history = "History"
     case computerScience = "Computer Science"
     case languages = "Languages"
@@ -25,13 +25,13 @@ enum PostCategory: String, Codable, CaseIterable {
     var icon: String {
         switch self {
         case .app: return "👩‍💻"
-     //   case .technology: return "💻"
+        case .technology: return "💻"
         case .science: return "🔬"
-      //  case .health: return "🏥"
+      case .health: return "🏥"
         case .business: return "💼"
         case .arts: return "🎨"
         case .math: return "🌟"
-     //   case .education: return "📚"
+        case .education: return "📚"
         case .history: return "📜"
         case .computerScience: return "💻"
         case .languages: return "🌍"
